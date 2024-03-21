b2eec5f4cf36f220f0cecb38a053dfe1d1a6faa7
sarmad <qadirbux.channar>
2024-03-22 01:24:10 +0500

    twake the text and added console at the end

--- index.ts
+++ index.ts
@@ -3,8 +3,8 @@
 import inquirer from "inquirer";
 
 const asnwer = await inquirer.prompt([
-  { message: "Enter first number", type: "number", name: "firstNumber" },
-  { message: "Enter second number", type: "number", name: "secondNumber" },
+  { message: "Enter your first number", type: "number", name: "firstNumber" },
+  { message: "Enter your second number", type: "number", name: "secondNumber" },
   {
     message: "select one of the operator to perform operation",
     type: "list",
@@ -25,3 +25,5 @@ if (asnwer.operator === "Addition") {
 } else {
     console.log("Please select valid operator")
 }
+
+console.log("The End")
